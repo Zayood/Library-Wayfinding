a1d41a2b24bd56953b8e261145c8d333412b6129
Zay <zghazal2001@gmail.com>
2020-11-27 12:06:59 -0500

    buttons functions

--- Main/core.js
+++ Main/core.js
@@ -1,10 +1,27 @@
 
 function goLanguage() {
-    location.href = "Language.html";
+
+    let page = getLocation();
+    var url;
+    if (page){
+    url = "Language.html?node=" + page;
+    }
+    else{
+        url = "Language.html";
+    }
+
+    location.href = url;
 }
 
 function goDestination() {
-    location.href = "Destination.html";
+
+    let page = getLocation();
+
+    href="../English Navigation/Landing_page_Front Entrance.html"
+
+    let url = "../English Navigation/Landing_page_" + page + ".html"
+
+    location.href = url;
 }
 
 function goDirections(){
@@ -13,4 +30,20 @@ function goDirections(){
 
 function reset(){
     location.href = "index.html";
+}
+
+function getLocation(){
+
+    var urlParams;
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+
+    return urlParams["node"];
 }
